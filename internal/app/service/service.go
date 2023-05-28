3009205fedf20474835c55018637d5550aaa68cc
Hryhoriy <dinozzzzzawrik@gmail.com>
2023-05-28 18:21:22 +0300

    change command to needed

--- internal/app/service/service.go
+++ internal/app/service/service.go
@@ -176,8 +176,8 @@ func (s *Service) Donate(body string) bool {
 			}
 		}(conn)
 
-		//_, err = conn.Execute(fmt.Sprintf("luckperms user %s parent add donate", donate.ClientName))
-		_, err = conn.Execute(fmt.Sprintf("say %s", donate.ClientName))
+		_, err = conn.Execute(fmt.Sprintf("luckperms user %s parent add donate", donate.ClientName))
+		//_, err = conn.Execute(fmt.Sprintf("say %s", donate.ClientName))
 
 		if err != nil {
 			panic(err)
